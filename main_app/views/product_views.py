c015098647d2e720bda146ed2f2723a107f16924
ohziiilov3 <klbaskerville0520@gmail.com>
2023-02-23 08:32:48 -0800

    product image view endpoint added

--- main_app/views/product_views.py
+++ main_app/views/product_views.py
@@ -45,9 +45,39 @@ def createProduct(request):
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
 
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateProduct(request, pk):
+    data= request.data
+    product = Product.objects.get(_id=pk)
+
+    product.name = data['name']
+    product.price = data['price']
+    product.make = data['make']
+    product.model = data['model']
+    product.countInStock = data['countInStock']
+    product.description= data['description']
+
+    product.save()
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
 @api_view(['DELETE'])
 @permission_classes([IsAdminUser])
 def deleteProduct(request,pk):
     product = Product.objects.get(_id=pk)
     product.delete()
     return Response('Product Deleted')
+
+@api_view(['POST'])
+def uploadImage(request):
+    data = request.data 
+    
+    product_id = data['product_id']
+    product = Product.objects.get(_id=product_id)
+
+    product.image = request.FILES.get('image')
+    product.save()
+    return Response('Image was uploaded')
